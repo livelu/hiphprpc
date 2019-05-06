0c0475e6c9ee251e5d707611231dd7e4c13eb340
张璐 <zhanglu@xinchanedu.com>
2019-05-07 00:16:44 +0800

    00

---
+++ src/hirpc/Example/ExampleClient.php
@@ -0,0 +1,32 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace hirpc\Example;
+
+/**
+ */
+class ExampleClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Example\GrpcRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function Call(\hirpc\Example\GrpcRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/example.Example/Call',
+        $argument,
+        ['\hirpc\Example\GrpcResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
