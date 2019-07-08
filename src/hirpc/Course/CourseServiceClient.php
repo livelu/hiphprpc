d3b4f52cc413bb72fcb579fd67fd5cf29eaca350
zhanglu@xinchanedu.com <zhanglu@xinchanedu.com>
2019-07-08 17:14:47 +0800

    course

---
+++ src/hirpc/Course/CourseServiceClient.php
@@ -0,0 +1,45 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Course;
+
+/**
+ */
+class CourseServiceClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Course\GetRequestOne $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetCurrRow(\Course\GetRequestOne $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/course.CourseService/GetCurrRow',
+        $argument,
+        ['\Course\CourseResponse', 'decode'],
+        $metadata, $options);
+    }
+
+    /**
+     * @param \Course\GetRequestList $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     */
+    public function GetCurrList(\Course\GetRequestList $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/course.CourseService/GetCurrList',
+        $argument,
+        ['\Course\CourseResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
